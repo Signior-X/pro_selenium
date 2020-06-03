c34ec5cccabe633825cb32012658bb497f9f133c
Signior-X <b19188@students.iitmandi.ac.in>
2020-06-03 13:11:22 +0530

    Added wp and insta

---
+++ insta.py
@@ -0,0 +1,93 @@
+#%% Import packages
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By 
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#%% Reading the file and importing random
+import pandas as pd
+import random
+
+names = pd.read_csv('unigram_freq.csv')['word']
+
+
+#%% Open chrome using driver
+# Replace below path with the absolute path 
+# to chromedriver in your computer 
+driver = webdriver.Chrome() 
+
+driver.get("https://instagram.com/") 
+wait = WebDriverWait(driver, 600) 
+
+#%% Login Form activity
+login_form = driver.find_element_by_tag_name('form')
+# print(login_form)
+
+unField = login_form.find_element_by_name('username')
+# print(unField)
+unField.send_keys(Keys.CONTROL + "a")
+unField.send_keys('sethpriyam1@gmail.com')
+
+pwField = login_form.find_element_by_name('password')
+# print(pwField)
+pwField.send_keys(Keys.CONTROL + "a")
+pwField.send_keys(os.getenv("INSTA_PASSWORD"))
+
+# Now submit
+pwField.send_keys(Keys.ENTER)
+
+# Proceed to run the next cell after waiting to complete it
+# Check how to wait for the page to complete loading
+#%% Remove the start badge
+not_now_xpath = '//*[@id="react-root"]/section/main/div/div/div/div/button'
+not_now_btn = driver.find_element_by_xpath(not_now_xpath)
+print(not_now_btn)
+not_now_btn.click()
+
+#%% Remove notification window if came up
+not_now_xpath = '/html/body/div[4]/div/div/div[3]/button[2]'
+not_now_btn = driver.find_element_by_xpath(not_now_xpath)
+print(not_now_btn)
+not_now_btn.click()
+
+#%% Open the messages
+chat_xpath = '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[2]'
+chat_btn = driver.find_element_by_xpath(chat_xpath)
+print(chat_btn)
+chat_btn.click()
+
+#%% Open chat of the person
+print("TO do later")
+
+#%% Send message
+message_area_xpath = '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea'
+message_area = driver.find_element_by_xpath(message_area_xpath)
+print(message_area)
+msg = "Get yourself someone, who fucks you like 2020!"
+count = int(input('Enter the count: '))
+message_area.send_keys(msg)
+message_area.send_keys(Keys.CONTROL + "a")
+message_area.send_keys(Keys.CONTROL + "c")
+message_area.send_keys(Keys.ENTER)
+for i in range(count):
+    rand_val = random.randint(0,1000)
+    message_area.send_keys(Keys.CONTROL + "v") 
+    message_area.send_keys(Keys.ENTER)
+print("Done")
+
+#%% Comment on a post (first set the post)
+comment_area = driver.find_element_by_class_name('X7cDz').find_element_by_tag_name('textarea')
+
+count = int(input("Enter the count: "))
+for i in range(count):
+    comment_area.send_keys("Awesome Pics bro!" + Keys.ENTER)
+    time.sleep(3)
+
+#%% Quit
+driver.quit()
--- web_whatsapp.py
+++ web_whatsapp.py
@@ -1,60 +1,60 @@
-from selenium import webdriver 
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.common.keys import Keys 
-from selenium.webdriver.common.by import By 
-import time 
-  
-# Replace below path with the absolute path 
-# to chromedriver in your computer 
-driver = webdriver.Chrome() 
-  
-driver.get("https://web.whatsapp.com/") 
-wait = WebDriverWait(driver, 600) 
+# %%
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
-# Replace 'Friend's Name' with the name of your friend  
-# or the name of a group  
+# %%
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = webdriver.Chrome()
+
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+# Replace 'Friend's Name' with the name of your friend
+# or the name of a group
 target = 'Friends'
 
-# Replace the below string with your own message 
+# Replace the below string with your own message
 msg = "Message sent using python!"
 input("Press Enter after scanning: ")
 
+# %% The main cell
 while(True):
-    try:
-        target = input("Enter the target (_cancel for stop) : ")
-        if(target == '_cancel'):
-            break
-        print("Target set")
 
-        # For the searching ans automatically selecting the first one
-        search_box = driver.find_element_by_class_name('_2S1VP')
-        search_box.send_keys(target + Keys.ENTER)
-        print(search_box)
-        time.sleep(1)
+    # target = input("Enter the target (_cancel for stop) : ")
+    target = "Rashmi"
+    input("Press Enter")
+    if(target == '_cancel'):
+        break
+    print("Target set")
 
-        main_content_holder = driver.find_element_by_tag_name('footer')
-        message_input = main_content_holder.find_element_by_class_name('_2S1VP')
+    # For the searching ans automatically selecting the first one
+    search_box = driver.find_element_by_class_name('_2S1VP')
+    search_box.send_keys(target + Keys.ENTER)
+    print(search_box)
 
-        msg = input("Enter the message to be sent: ")
-        count = int(input("Enter the count: "))
+    main_content_holder = driver.find_element_by_tag_name('footer')
+    message_input = main_content_holder.find_element_by_class_name('_2S1VP')
 
-        for i in range(count):
-            message_input.send_keys((msg+' '+str(i)) + Keys.ENTER)
+      # msg = input("Enter the message to be sent: ")
+      # msg = "test"
 
-        print("Messages sent!")
-    except:
-        print("Aborting, try new one!")
+    count = int(input("Enter the count: "))
+
+    for i in range(count):
+        message_input.send_keys(Keys.CONTROL + "v")
+        message_input.send_keys(' '+str(i))
+        message_input.send_keys(Keys.ENTER)
+    
+
+    print("Messages sent!")
+    
 print("Done")
-'''
-# For the tabs
-x_arg = '//span[contains(@title,' + target + ')]'
-group_title = driver.find_element_by_class_name('_2EXPL')
-group_title.click()
-'''
+# Nirlaj to vo hai jo kaayaro ki bhati yudh chhor ke bhag jaate hai
 
-'''
-for i in range(100): 
-    input_box.send_keys(string + Keys.ENTER) 
-    time.sleep(1)
-'''
+# %% Quit
+driver.quit()
